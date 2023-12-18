1a3cdd1923d111eaf9d7a2c65112e0250380d62b
João Vitor Sousa <joaovitorsousa@Joaos-MacBook-Pro.local>
2023-12-18 16:07:53 -0300

    project setup

---
+++ reMind/reMind/App/reMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  reMindApp.swift
+//  reMind
+//
+//  Created by João Vitor Sousa on 18/12/23.
+//
+
+import SwiftUI
+
+@main
+struct reMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
